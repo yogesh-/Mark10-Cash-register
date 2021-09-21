22384454181adba2a9bbcaccab65a5f882ef6ed6
Yogesh Gupta <yogesh.gupta38@gmail.com>
2021-09-21 17:34:36 +0530

    some error

---
+++ index.js
@@ -0,0 +1,7 @@
+const totalBill = document.querySelector("#total-bill");
+const cashGiven = document.querySelector("#cash-given");
+const calcButton = document.querySelector("#calculate");
+
+// 
+
+totalBill.addEventListener('input',console.log(totalBill.value));
