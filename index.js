082e3fba0e1e54db0ab6dbdfa7d189fc6c4f44e0
Yogesh Gupta <yogesh.gupta38@gmail.com>
2021-09-21 23:54:05 +0530

    .

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const totalBill = document.querySelector("#total-bill");
 const cashGiven = document.querySelector("#cash-given");
 const calcButton = document.querySelector("#calculate");
-const message = document.querySelector("error-message");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelector(".no-of-notes");
 
-
+const availableDenominations = [2000,500,100,20,10,5,1];
 
 calcButton.addEventListener("click",function inputValidation() {
-    if  (totalBill > 0){
-        if (cashGiven >= totalBill){
-             const differenceValue = cashGiven - totalBill;
+     hideMessage();
+    if  (totalBill.value > 0){
+        if (cashGiven.value >= totalBill.value){
+             const differenceValue = cashGiven.value - totalBill.value;
+             calculateChange(differenceValue);
              console.log(differenceValue);
         }else{
             showMessage("Dear customer you need to pay in full");
@@ -19,6 +22,20 @@ calcButton.addEventListener("click",function inputValidation() {
     }
 });
 
+//200/1000 = 800
+
+function calculateChange(differenceValue){
+    for (i=0;i< availableDenominations.length;i++){
+        const numberOfNotes = Math.trunc(differenceValue / availableDenominations[i]);
+        differenceValue = differenceValue % availableDenominations[i]; 
+        noOfNotes[i].innerText = numberOfNotes; 
+    }
+
+}
+
+function hideMessage(){
+    message.style.display = "none";
+}
 
 function showMessage(msg){
     message.style.display = "block";
