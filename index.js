5cef8c01b91d552a595d7401e5f7c1c087999ffc
Yogesh Gupta <yogesh.gupta38@gmail.com>
2021-09-21 20:00:41 +0530

    input validation

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const totalBill = document.querySelector("#total-bill");
 const cashGiven = document.querySelector("#cash-given");
 const calcButton = document.querySelector("#calculate");
+const message = document.querySelector("error-message");
 
-// 
 
-totalBill.addEventListener('input',console.log(totalBill.value));
+
+calcButton.addEventListener("click",function inputValidation() {
+    if  (totalBill > 0){
+        if (cashGiven >= totalBill){
+             const differenceValue = cashGiven - totalBill;
+        }else{
+            showMessage("Dear customer you need to pay in full");
+        }
+
+    }else{
+        showMessage("Invalid Bill Amount");
+    }
+});
+
+
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerText = msg;
+}
